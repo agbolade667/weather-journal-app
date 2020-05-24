e065be6b34fdb13c171a4aa0c5802bd5e373aeb2
Agbolade Omowole <agboladeomowole@gmail.com>
2020-05-24 21:41:17 +0100

    Feat: Updated server.js by adding dependencies and starting the server

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
-// Setup empty JS object to act as endpoint for all routes
+/* Empty JS object to act as endpoint for all routes */
 projectData = {};
 
-// Require Express to run server and routes
+// Express to run server and routes
+const express = require('express');
 
-// Start up an instance of app
+// Start up Express - an instance of Express 
+const app = express();
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+// Body-parser dependency required 
+const bodyParser = require('body-parser');
+
+// Addition of Bodyparser; Here we are configuring express to use body-parser as middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+// Cors is included for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
-// Initialize the main project folder
-app.use(express.static('website'));
+// Initialization of the main project folder
+app.use(express.static('weather-journal-app'));
 
+const port = 8000;
+// Start up of the server
 
-// Setup Server
+const server = app.listen(port, listening());
+
+// Callback function is added 
+
+function listening() {
+  console.log('server running');
+  console.log(`running on location: ${port}`);
+}
